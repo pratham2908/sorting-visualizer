0921327f5c4cecf046dc08f7a2e473d6ba52808c
Pratham Garg <aggarwal.prathamgarg@gmail.com>
2022-11-13 14:21:40 +0530

    Pushing App code on Github for first time

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import BarContainer from './Components/BarContainer';
+import SortButtons from './Components/SortButtons';
+import BarContextProvider from './Components/BarContext';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BarContextProvider>
+        <SortButtons />
+        <BarContainer />
+      </BarContextProvider>
     </div>
   );
 }
